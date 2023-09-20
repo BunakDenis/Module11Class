e4df633b7895f4d9048a1457adffac782dd3aa2c
Denis Bunak <xisi926@ukr.net>
2023-09-20 13:41:09 +0300

    Add maven to project

--- src/main/global/goit/edu/module10/StringListTest.java
+++ src/main/global/goit/edu/module10/StringListTest.java
@@ -1,5 +1,7 @@
 package main.global.goit.edu.module10;
 
+import com.google.gson.Gson;
+
 import java.util.Arrays;
 
 public class StringListTest {
@@ -7,5 +9,6 @@ public class StringListTest {
     public static void main(String[] args) {
         String[] strings = new String[]{};
         System.out.println("strings = " + Arrays.toString(strings));
+
     }
 }
